6e5e821ad27161fcac43787c25485237cab23834
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-08 19:02:43 +0200

    wip

--- api/users/user-model.js
+++ api/users/user-model.js
@@ -4,6 +4,7 @@ const db = require('../../data/db-config')
 module.exports = {
   async getPostsBy(user_id) {
     const result = await db('posts as p')
-      .join('users as u', 'p.user_id', '=', 'u.id') // the '
+      .join('users as u', 'p.user_id', '=', 'u.id') // the '=' is optional
+      .select('p.id as post_id', 'contents')
   }
 }
